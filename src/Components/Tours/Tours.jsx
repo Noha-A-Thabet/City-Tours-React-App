c08576e9d6363ba40a9c1e4560c9900cd476931d
Noha-A-Thabet <NohaAshraf25@outlook.com>
2023-02-04 11:22:13 +0200

    adding sass files

---
+++ src/Components/Tours/Tours.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./Tours.css";
+
+export default function Tours() {
+  return <div>Tours</div>;
+}
---
+++ src/Components/ToursList/ToursList.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./ToursLIst.css";
+
+export default function ToursList() {
+  return <div>tours list</div>;
+}
